f146f13bdf39ce26a3545a3e9aea2fb1b074e5a9
ishammohamed <mail.to.isham@gmail.com>
2021-03-03 17:10:23 +0530

    added navbar, post and card componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostsComponent } from './posts/posts.component';
+import { CardComponent } from './posts/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostsComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
